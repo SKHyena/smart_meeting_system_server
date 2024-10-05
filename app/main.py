fa50584182149b6cd7b2bff81908153bf9bf8ca7
kyunghwan <dnjsdos@gmail.com>
2024-10-06 01:00:56 +0900

    ensure ascii

--- app/main.py
+++ app/main.py
@@ -77,7 +77,7 @@ async def reserve(
             raise HTTPException(status_code=500, detail=f"File upload failed: {str(e)}")
         
     meeting_info: dict[str, Any] = json.loads(reserve_data)
-    meeting_info["files"] = json.dumps(files_info)
+    meeting_info["files"] = json.dumps(files_info, ensure_ascii=False)
     db_manager.insert_meeting_table(meeting_info)
 
     attendees: List[dict[str, Any]] = json.loads(attendees_data)
